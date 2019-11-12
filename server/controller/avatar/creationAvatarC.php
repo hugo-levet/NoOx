79d4ab6c63f915594ea4a41f6e8812e86376bd6e
LEVET Hugo <l18023836@A-F002078.salsa.univ-amu.fr>
2019-11-12 10:21:11 +0100

    creation avatar 0.1

--- server/controller/avatar/creationAvatarC.php
+++ server/controller/avatar/creationAvatarC.php
@@ -1,8 +1,30 @@
 <?php
-    /*
-        title : creationAvatarC.php 
-        author : Hugo.L
-        started on : 
-        brief : controller page creation d'un avatar 
-    */
+/*
+    title : creationAvatarC.php
+    author : Hugo.L
+    started on : 10/11/2019
+    brief : controller page creation d'un avatar
+*/
+class creationAvatarC
+{
+    function _construct()
+    {
+
+    }
+
+    public function formVerification($data)
+    {
+        //vérification nom : pas trop court pas trop long et uniquement composé de lettres et quelques espaces
+        //vérification race
+        //vérification trait raciaux
+        //vérification constitution
+        //vérification caractéristiques
+        //vérification compétences
+        //vérification traits
+        //vérification implant
+        //vérification niveau de vie
+        //vérification génération
+        //vérification points de création
+    }
+}
 ?>
