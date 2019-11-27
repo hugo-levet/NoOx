086800ab8ba267a35f97fb57b4012dab31c86d52
Hugo Panis <hugopanis@MacBook-Pro-de-Hugo.local>
2019-11-27 13:50:48 +0100

    profile view terminé

--- public/view/user/profileV.php
+++ public/view/user/profileV.php
@@ -6,57 +6,108 @@
         brief : view page profile
     */
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil | NoOx</title>
 </head>
 <body>
-    <!-- Modifier de mot de passe -->
-    <form method="post" action="#">
-        <fieldset>
-            <legend>modification mot de passe</legend>
-            <div>
-                <label for="ancienMdp">Ancien mot de passe :</label><input type="password" name="ancienMdp">
+<header>
+    <div id="HeaderContainer"></div>
+</header>
+<main>
+    <section id="ProfileContainer">
+        <div id="ProfileTitle"> Mon profil </div>
+        <div id="ListProfile">
+            <div class="ProfileLine">
+                <p class="CategoryName"> Nom </p>
+                <p class="Value"><?php //getSurname(); ?></p>
             </div>
-            <div>
-                <label for="nouveauMdp">Nouveau mot de passe :</label><input type="password" name="nouveauMdp">
+            <div class="ProfileLine">
+                <p class="CategoryName"> Prénom </p>
+                <p class="Value"><?php //getName(); ?></p>
             </div>
-            <div>
-                <label for="nouveauMdpBis">Répéter votre mot de passe :</label><input type="password" name="nouveauMdpBis">
+            <div class="ProfileLine">
+                <p class="CategoryName"> Pseudo </p>
+                <p class="Value"><?php //getPseudo() ?></p>
             </div>
-            <button type="submit" name="modificationMdp" value="modificationMdp">modifier</button>
-        </fieldset>
-    </form>
-
-    <!-- Modifier de pseudo -->
-    <form method="post" action="#">
-        <fieldset>
-            <legend>modification pseudo</legend>
-            <div>
-                <label for="ancienPseudo">Pseudo actuel :</label>
+            <div class="ProfileLine">
+                <p class="CategoryName"> e-mail </p>
+                <p class="Value"><?php //getMail() ?></p>
             </div>
-            <div>
-                <label for="nouveauPseudo">Nouveau Pseudo :</label><input type="text" name="nouveauPseudo">
+            <div class="ProfileLine">
+                <p class="CategoryName"> Mot de passe </p>
+                <p class="Value"><?php //getPassword() ?></p>
             </div>
-            <button type="submit" name="modificationPseudo" value="modificationPseudo">modifier</button>
-        </fieldset>
-    </form>
-
-    <!-- Modifier de Mail -->
-    <form method="post" action="#">
-        <fieldset>
-            <legend>modification mail</legend>
-            <div>
-                <label for="ancienMdp">Adresse mail actuelle :</label>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Statut </p>
+                <p class="Value"><?php //getStatus() ?></p>
             </div>
-            <div>
-                <label for="nouveauMail">Nouvelle adresse mail :</label><input type="email" name="nouveauMail">
+            <div class="ProfileLine">
+                <p class="CategoryName"> Rang communautaire </p>
+                <p class="Value"><?php //getRank() ?></p>
             </div>
-            <button type="submit" name="modificationMail" value="modificationMail">modifier</button>
-        </fieldset>
-    </form>
-
-    <form class="deconnexion" action="" method="post">
-        <button class="deconnexion" type="submit" name="deconnexion" value="deconnexion">se deconnecter</button>
-    </form>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Langue </p>
+                <p class="Value"><?php //getLanguage() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Adresse </p>
+                <p class="Value"><?php //getAdresse() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Téléphone </p>
+                <p class="Value"><?php //getPhone() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Date de naissance </p>
+                <p class="Value"><?php //getBirthDate() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Site web </p>
+                <p class="Value"><?php //getWebSite() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Première connexion </p>
+                <p class="Value"><?php //getFirstConnexion() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Dernière connexion </p>
+                <p class="Value"><?php //getLastConnexion() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Portrait </p>
+                <p class="Value"><?php //getPortrait() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Civilité </p>
+                <p class="Value"><?php //getCivility() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Signature </p>
+                <p class="Value"><?php //getSignature() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Présentation </p>
+                <p class="Value"><?php //getPresentation() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Fuseau horaire </p>
+                <p class="Value"><?php //getTimeZone() ?></p>
+            </div>
+            <div class="ProfileLine">
+                <p class="CategoryName"> Devise </p>
+                <p class="Value"><?php //getCurrency() ?></p>
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+</footer>
 </body>
 </html>
