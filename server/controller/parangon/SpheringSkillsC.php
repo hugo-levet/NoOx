7bc594522f81214c25d7e14abb4817590fb4e102
hugolevet <hugo.levet42130@gmail.com>
2019-12-03 18:06:06 +0100

    sphérier image map

---
+++ server/controller/parangon/SpheringSkillsC.php
@@ -0,0 +1,16 @@
+<?php
+/*
+title : spheringSkillsV.php
+author : Hugo.L
+started on : 03/12/2019
+brief : sphering skills controller
+*/
+require_once('./server/controller/ControllerC.php');
+class SpheringSkills extends Controller
+{
+    function __construct($url)
+    {
+        
+    }
+}
+?>
