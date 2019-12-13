f9320914d1156be096ceb36714eff51e7973861f
hugolevet <hugo.levet42130@gmail.com>
2019-12-13 15:22:37 +0100

    template Audrey

--- public/view/template/template.php
+++ public/view/template/template.php
@@ -15,18 +15,74 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="./../public/assets/css/templateG.css">
+    <?php
+        // for ( $i= 0 ; $i < sizeof ($listStyle); ++$i){
+        //     echo '<link rel="stylesheet" href"./../public/assets/css/'.$listStyle[$i].'.css">';
+        // }
+        // for ($i = 0; $i < sizeof($listJS); ++$i) {
+        //     echo '<script src="./..///js/'.$listJS[$i].'.js" defer></script>';
+        // }//??
+    ?>
+
     <?= '<link rel="stylesheet" href="./../public/assets/css/' . $category . '/' . lcfirst($url[1]) . '.css">'; ?>
 
     <script src="https://kit.fontawesome.com/b18ab37082.js" crossorigin="anonymous"></script>
-    
+    <script src="./../public/assets/js/template.js" defer></script>
+
     <title>NoOx</title>
 </head>
 <body>
     <section id="logoNavContainer">
+        <button id="navbarTrigger" class="buttonNavND">
+            <i class="fas fa-bars"></i>
+        </button>
+
         <!-- logo -->
         <img src="./../public/assets/other/NoOxLogo.png" id="logoTopImg" alt="NoOx logo">
     </section>
-    
+
+    <!-- si utilisateur connecté -->
+    <section id="header" class="navND">
+        <nav id="navbarContainer">
+
+            <div id="navUser">
+                <label for="navUser" class="navLabel">Utilisateur</label>
+
+                <?php
+                    if (!isset($_SESSION['id'])) {
+                        echo '<a href="" class="navLink"><i class="fas fa-sign-in-alt"></i> S\'inscrire</a>';
+                        echo '</div>';
+                    }
+                    else {
+                ?>
+                    <a href="" class="navLink"> <i class="far fa-user"></i> Mon profil</a>
+                    <a href="" class="navLink"> <i class="fas fa-sign-out-alt"></i> Deconnexion</a>
+                </div>
+
+                <div id="navParangon">
+                    <label for="navParangon" class="navLabel">Paragons</label>
+                    <a href="" class="navLink"><i class="far fa-eye"></i> Mes Parangons</a>
+                </div> 
+
+                <div id="navAvatar">
+                    <label for="navAvatar" class="navLabel">Avatars</label>
+                    <a href="" class="navLink"><i class="far fa-eye"></i> Mes Avatars</a>
+                </div>
+
+                <div id="navScenario">
+                    <label for="navScenario" class="navLabel">Scénarios</label>
+                    <a href="" class="navLink"><i class="far fa-file-alt"></i> Mes scénarios</a>
+                </div>
+                    <?php
+                        }
+                    ?>
+
+            </nav>
+        <div id="footerContainer">
+            2019 - NoOx
+        </div>
+    </section>
+
     <?= require_once($fileView); ?>
     <script type="text/javascript">
         document.title = "<?=$title;?> | NoOx"
