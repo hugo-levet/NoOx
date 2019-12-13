3aeda7378833def56fb187da18a277064b2ae638
hugolevet <hugo.levet42130@gmail.com>
2019-12-13 10:46:30 +0100

    config

--- server/config.php
+++ server/config.php
@@ -5,8 +5,8 @@
     started on : 27/11/2019
     brief : configuration variables
 */
-$host  = 'localhost';
-$databaseName = 'testnoox';
-$databaseId = 'root';
-$databasePassword = '';
+$host  = 'mysql-projet-noox.alwaysdata.net';
+$databaseName = 'projet-noox_database';
+$databaseId = '193036';
+$databasePassword = 'Projetnoox';
 ?>
