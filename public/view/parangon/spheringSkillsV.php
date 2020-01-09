4b7ba0e7660ce7bac824f9dba2f956790eddf32f
hugolevet <hugo.levet42130@gmail.com>
2020-01-09 13:47:26 +0100

    zoom shpérier de compétences

--- public/view/parangon/spheringSkillsV.php
+++ public/view/parangon/spheringSkillsV.php
@@ -67,6 +67,74 @@ $title = 'Sphérier de compétences';
                 <button id="plus">+</button>
             </div>
         </div>
+        <div id="controlButton">
+                <button id="zoom" onclick="zoom()">+</button>
+                <button id="zoomOut" onclick="zoomOut()">-</button>
+        </div>
+        <script>
+            spheringSkillsImg = document.getElementById('spheringSkillsImg');
+            spheringSkillsImg.width = window.innerWidth;
+            
+        function changeCoordsArea()
+        {
+            //modifier chaque coordonnée de la map de l'image
+            
+            //recuperer les coords en json
+            var requestURL = '<?= $this->getRootReturn(); ?>public/assets/js/skills.json';
+            var request = new XMLHttpRequest();
+            request.open('GET', requestURL);
+            request.responseType = 'json';
+            request.send();
+            request.onload = function() {
+                areaJson = request.response;
+
+                resolution = spheringSkillsImg.width / 1587;
+                //for tous les area
+                for(var i = 0; i < document.getElementsByName('image-map')[0].children.length-1; i++)
+                {
+                    aArea = document.getElementsByName('image-map')[0].children[i];
+                    //on prend se qui correspond en json
+                    aAreaJson = areaJson[i];
+
+                    coords = aAreaJson['coords'].split(',');
+                    //on y multiplie par la resolution
+                    coords.forEach(function(element, index){
+                        coords[index] = Math.floor(element * resolution);
+                    });
+                    //on y remet dans coords
+                    aArea.coords = coords.join(',');
+                }
+            }
+        }
+
+        function zoom()
+        {
+            console.log('zoom');
+            spheringSkillsImg.width *= 1.16666;
+
+            //modifier chaque coordonnée de la map de l'image
+            changeCoordsArea();
+        }
+
+        function zoomOut()
+        {
+            console.log('zoomOut');
+            spheringSkillsImg.width *= 0.85714775513;
+
+            if(spheringSkillsImg.width < window.innerWidth)
+            {
+                spheringSkillsImg.width = window.innerWidth;
+                //modifier chaque coordonnée de la map de l'image
+                changeCoordsArea();
+            }
+            else
+            {
+                //modifier chaque coordonnée de la map de l'image en mettant ceux de l'image de base TODO
+                changeCoordsArea();
+            }
+        }
+        </script>
+
 
         <script src="<?= $this->getRootReturn(); ?>public\assets\js\ajax.js"></script>
         <script src="<?= $this->getRootReturn(); ?>public\assets\js\skills.js"></script>
