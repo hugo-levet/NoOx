8088e3409b090ebeb73e253ede773afa79829ff0
hugo-panis <hugo.panis@etu.univ-amu.fr>
2020-01-12 21:36:21 +0100

    homepage clean

--- public/view/homePageV.php
+++ public/view/homePageV.php
@@ -10,20 +10,20 @@
     <p>
         <ul>
             <li>
-                Cette page web est destiné à l’univers de NoOx, un jeu de rôle se déroulant
-                dans un endroit fantastique avec des personnages unique en son genre.
+                Cette page web est destinée à l’univers de NoOx, un jeu de rôle se déroulant
+                dans un endroit fantastique avec des personnages uniques en leur genre.
             </li>
             <li>
-                Vous pouvez configurer leurs attributs tel que leur charisme pour être le plus beau
-                ou la plus belle dans NoOx, et aussi leur métabolisme pour devenir invincible fasse aux maladies.
+                Vous pouvez configurer leurs attributs tels que leur charisme pour être le plus beau
+                ou la plus belle dans NoOx, et aussi leur métabolisme pour devenir invincible face aux maladies.
             </li>
             <li>
-                Mais ce n'est pas tout tu peux aussi devenir un Dieu Sensus,
-                une Ombre de la mort.
+                Mais ce n'est pas tout, vous pouvez aussi devenir un Dieu Sensus,
+                une ombre de la mort.
             </li>
             <li>
-                Et cela n’est qu’un avant-goût, pour pouvoir réellement
-                profiter de cet univers veuillez soit vous
+                Et cela n’est qu’un avant-goût. Pour pouvoir réellement
+                profiter de cet univers, veuillez soit vous
                 <a href="user/Login"><button type="button">connecter</button></a>
                 soit vous
                 <a href="user/Register"><button type="button">inscrire</button></a>
